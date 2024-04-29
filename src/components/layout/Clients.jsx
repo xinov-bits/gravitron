42b7aa2ec065cbd0d7a4c18f9ed842bfa4c6770b
Tanishq Solanki <officialxinov@gmail.com>
2024-04-30 00:42:22 +0530

    inital commit

--- src/components/layout/Clients.jsx
+++ src/components/layout/Clients.jsx
@@ -31,7 +31,7 @@ const Clients = () => {
         <>
             <div className="flex justify-center items-center w-full h-full bg-[--black-1100]">
                 <div className="flex justify-center items-center w-screen h-16 mt-10 bg-[--black-1100] text-[--white-100] !cursor-pointer">
-                    <Splide className="flex justify-center items-center !size-full !cursor-pointer overflow-hidden"
+                    <Splide className="hidden sm:hidden md:flex lg:flex xl:flex justify-center items-center !size-full !cursor-pointer overflow-hidden"
                         options={{
                             type: 'loop',
                             drag: false,
@@ -44,7 +44,37 @@ const Clients = () => {
                             },
                             arrows: false,
                             pagination: false,
-                            perPage: window !== undefined ? (window?.innerWidth <= 600 ? (window?.innerWidth <= 400 ? 3.5 : 5) : 7) : 7,
+                            perPage: 7,
+                            gap: "20px",
+                        }}
+                        extensions={{ AutoScroll }}
+                    >
+                        {clients.map((item, index) => (
+                            <SplideSlide key={index} className="flex justify-center items-center h-full py-4">
+                                <Image className="flex justify-center items-center h-full oapcity-95"
+                                    src={item}
+                                    width={200}
+                                    height={32}
+                                    alt=""
+                                />
+                            </SplideSlide>
+                        ))}
+                    </Splide>
+
+                    <Splide className="flex sm:flex md:hidden lg:hidden xl:hidden justify-center items-center !size-full !cursor-pointer overflow-hidden"
+                        options={{
+                            type: 'loop',
+                            drag: false,
+                            focus: 'center',
+                            autoScroll: {
+                                autoStart: true,
+                                speed: 2,
+                                pauseOnHover: false,
+                                pauseOnFocus: false,
+                            },
+                            arrows: false,
+                            pagination: false,
+                            perPage: 3.5,
                             gap: "20px",
                         }}
                         extensions={{ AutoScroll }}
